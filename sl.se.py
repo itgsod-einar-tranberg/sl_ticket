0c09cf76bd68bb7072fd3d5993b1756caf2ec555
einar.tranberg <itgsod-einar-tranberg>
2017-10-25 13:06:49 +0200

    Commit och push av sl ticket.

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,23 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age, senior=False, student=False,adult=False, discount=False):
+    if age > 20 and age < 65:
+        if adult==True:
+            return "Fullprice 30kr"
+        else:
+            return "discount price 20kr"
+    elif age<=20 or age >=65:
+            if student==True:
+                return "Discount price 20kr"
+            elif senior==True:
+                return "Discount price 20kr"
+
+    print "error"
 
 
 
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
+print fares(10)
+print fares(65,senior=True)
+print fares(20,student=True)
+print fares(64,adult=True)
